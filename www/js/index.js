ca29ceb276b91272eeaca7d8ae6557cbcbc4b804
James Cox <th3james@gmail.com>
2012-11-29 17:50:34 +0000

    Make loads after downloading MBTiles

--- www/js/index.js
+++ www/js/index.js
@@ -1,6 +1,13 @@
+window.onerror = function(message, url, linenumber) {
+  alert("JavaScript error: " + message + " on line " + linenumber + " for " + url);
+  console.log("JavaScript error: " + message + " on line " + linenumber + " for " + url);
+};
 var app = {
   // Application Constructor
   initialize: function() {
+    this.localFileName =  'bluecarbon.mbtiles';
+    this.remoteFile =  'https://dl.dropbox.com/s/jllnjvlwvhfqys1/test.mbtiles';
+
     this.bindEvents();
   },
   // Bind Event Listeners
@@ -8,23 +15,49 @@ var app = {
   // Bind any events that are required on startup. Common events are:
   // 'load', 'deviceready', 'offline', and 'online'.
   bindEvents: function() {
-    document.addEventListener('deviceready', this.onDeviceReady, false);
+    var _this = this;
+    document.addEventListener('deviceready', function() {
+      _this.onDeviceReady();
+    }, false);
   },
   // deviceready Event Handler
   //
   // The scope of 'this' is the event. In order to call the 'receivedEvent'
   // function, we must explicity call 'app.receivedEvent(...);'
   onDeviceReady: function() {
-   // create a map in the "map" div, set the view to a given place and zoom
-    var map = L.map('map').setView([51.505, -0.09], 13);
+    var _this = this;
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+      fs = fileSystem;
 
-    // add a CloudMade tile layer with style #997
-    L.tileLayer('http://{s}.tile.cloudmade.com/[API-key]/997/256/{z}/{x}/{y}.png', {
-        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>'
-        }).addTo(map);
+      var file = fs.root.getFile(_this.localFileName, {create: false}, function () {
+        _this.buildMap();
+      }, function () {
+        console.log('Downloading file...');
 
-    // add a marker in the given location, attach some popup content to it and open the popup
-    L.marker([51.5, -0.09]).addTo(map)
-      .bindPopup('A pretty CSS3 popup. <br> Easily customizable.').openPopup(); 
+        ft = new FileTransfer();
+        ft.download(_this.remoteFile, fs.root.fullPath + '/' + _this.localFileName, function (entry) {
+          _this.buildMap();
+        }, function (error) {
+          alert('GError');
+          console.log(error);
+        });
+      });
+    });
+  },
+
+  buildMap: function() {
+    var db = window.sqlitePlugin.openDatabase(this.localFileName, "1.0", "Tiles", 2000000);
+
+    this.map = new L.Map('map', {
+      center: new L.LatLng(24.2870, 54.3274),
+        zoom: 9
+    });
+
+    tileLayer = new L.TileLayer.MBTiles(db, {
+      tms: true
+    }).addTo(this.map);
+
+    //var polygonDraw = new L.Polygon.Draw(map, {});
+    //polygonDraw.enable();
   }
 };
